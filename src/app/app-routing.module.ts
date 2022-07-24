3a7bcd338da74c23ed29d2989b733367f931205b
jsvanderlaan <js.vanderlaan@live.nl>
2022-07-24 07:53:05 +0200

    replaced scrolling with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,59 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
+import { Q1Component } from './q1/q1.component';
+import { Q2Component } from './q2/q2.component';
+import { Q3Component } from './q3/q3.component';
+import { Q4Component } from './q4/q4.component';
 
-const routes: Routes = [];
+export const routeColors = {
+    intro: 'bg-orange-300',
+    q1: 'bg-amber-300',
+    q2: 'bg-teal-300',
+    q3: 'bg-cyan-300',
+    q4: 'bg-fuchsia-300',
+};
+
+const routes: Routes = [
+    {
+        path: 'intro',
+        component: IntroComponent,
+        data: {
+            backgroudColor: routeColors.intro,
+        },
+    },
+    {
+        path: '1',
+        component: Q1Component,
+        data: {
+            backgroudColor: routeColors.q1,
+        },
+    },
+    {
+        path: '2',
+        component: Q2Component,
+        data: {
+            backgroudColor: routeColors.q2,
+        },
+    },
+    {
+        path: '3',
+        component: Q3Component,
+        data: {
+            backgroudColor: routeColors.q3,
+        },
+    },
+    {
+        path: '4',
+        component: Q4Component,
+        data: {
+            backgroudColor: routeColors.q4,
+        },
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { routeColors } from './app-routing.module';
 
 @Component({
     selector: 'app-root',
@@ -6,7 +8,13 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-    test(e: any): void {
-        e.preventDefault();
+    activeColor: string | null = null;
+    routeColors = routeColors;
+    constructor(router: Router) {
+        router.events.subscribe(e => {
+            if (e instanceof ActivationEnd) {
+                this.activeColor = e.snapshot.data['backgroudColor'];
+            }
+        });
     }
 }
